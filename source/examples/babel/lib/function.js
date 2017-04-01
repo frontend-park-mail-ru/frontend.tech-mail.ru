c1529aa7ddd786c5d0d65abd7e0b898ba5343139
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2017-04-01 14:12:11 +0300

    Add examples

--- source/examples/babel/lib/function.js
+++ source/examples/babel/lib/function.js
@@ -4,6 +4,12 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
-exports.default = function (...strings) {
-	console.log([...new Set(strings).values()]);
+exports.default = function () {
+	for (var _len = arguments.length, strings = Array(_len), _key = 0; _key < _len; _key++) {
+		strings[_key] = arguments[_key];
+	}
+
+	console.log([].concat(_toConsumableArray(new Set(strings).values())));
 };
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
