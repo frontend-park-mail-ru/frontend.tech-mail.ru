29ec963634629f9878fd16421642ce22ca6c27b8
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2017-10-19 23:43:14 +0300

    Examples

---
+++ ex/generators/async.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const fs = require('fs');
+
+function read(file) {
+	return new Promise(function (resolve, reject) {
+		fs.readFile(file, 'utf8', function (err, text) {
+			if (err) {
+				return reject(err);
+			}
+			resolve(text);
+		});
+	});
+}
+
+function write(file, source) {
+	return new Promise(function (resolve, reject) {
+		fs.writeFile(file, source, 'utf8', function (err) {
+			if (err) {
+				return reject(err);
+			}
+			resolve();
+		});
+	});
+}
+
+const work = async function () {
+	const list = await read('./files/list.dat');
+	const filenames = list.split('\n').map(name => name.trim()).filter(name => !!name);
+
+	const nums = await Promise.all(
+		filenames.map(filename => read('./files/' + filename))
+	);
+
+	const summ = nums.reduce((sum, num) => + num + + sum);
+
+	await write('result-async.dat', `Result is ${summ}`);
+};
+
+work()
+	.then(() => console.log('FINISH!'))
+	.catch(console.error);
---
+++ ex/generators/dummy.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+
+fs.readFile('./files/list.dat', 'utf8', function (err, text) {
+	if (err) {
+		return console.error(err);
+	}
+	const [filename1, filename2, filename3, filename4] = text.split('\n').map(name => name.trim()).filter(name => !!name);
+	let summ = 0;
+
+	fs.readFile('./files/' + filename1, 'utf8', function (err, text) {
+		if (err) {
+			return console.error(err);
+		}
+		summ +=+ text;
+		fs.readFile('./files/' + filename2, 'utf8', function (err, text) {
+			if (err) {
+				return console.error(err);
+			}
+			summ +=+ text;
+			fs.readFile('./files/' + filename3, 'utf8', function (err, text) {
+				if (err) {
+					return console.error(err);
+				}
+				summ +=+ text;
+				fs.readFile('./files/' + filename4, 'utf8', function (err, text) {
+					if (err) {
+						return console.error(err);
+					}
+					summ +=+ text;
+
+
+					fs.writeFile('result-dummy.dat', `Result is ${summ}`, 'utf-8', function (err) {
+						if (err) {
+							return console.error(err);
+						}
+
+						console.log('FINISH!');
+						process.exit();
+					});
+				});
+			});
+		});
+	});
+});
